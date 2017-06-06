c719d0e1980544eb53d46bcf0f203bf8e04e0c28
saikiaki <saikiaki@mori-system.co.jp>
2017-06-06 18:09:06 +0900

    "use-packageを使うように修正"

--- init.el
+++ init.el
@@ -2,7 +2,14 @@
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/") t)
 ;;インストールしたパッケージにロードパスを通してロードする
+(setq load-prefer-newer t)
 (package-initialize)
+(require 'use-package)
+;; auto compile elisp
+(use-package auto-compile
+  :config
+  (auto-compile-on-load-mode 1)
+  (auto-compile-on-save-mode 1))
 ;; 自作にスクリプト群
 (add-to-list 'load-path (concat user-emacs-directory "site-lisp"))
 ;; custom variablesの値をロード
@@ -12,81 +19,102 @@
 ;; 基本的な設定
 (load "common")
 
-;; auto compile elisp
-(when (require 'auto-compile)
-  (auto-compile-on-load-mode 1)
-  (auto-compile-on-save-mode 1))
-
 ;; wdired
-(when (require 'wdired)
-  (define-key dired-mode-map "r" 'wdired-change-to-wdired-mode)
-  (global-set-key (kbd "C-x C-d") 'dired))
+(use-package wdired
+  :bind (
+		 ("C-x C-d" . dired)
+		 :map dired-mode-map
+			  ("r" . wdired-change-to-wdired-mode)
+		 ))
 
 ;; yasnippet
-(yas-global-mode 1)
-;; (global-yascroll-bar-mode 1)
+(use-package yasnippet
+  :diminish yas-minor-mode
+  :config
+  (yas-global-mode t))
 
 ;; junk file
-(require 'open-junk-file)
+(use-package open-junk-file)
 
 ;; markdown
-(autoload 'markdown-mode "markdown-mode"
-   "Major mode for editing Markdown files" t)
-(add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
-(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(use-package markdown-mode
+  :commands markdown-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+  (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode)))
 
 ;; rainbow-delimiters
-(when (require 'rainbow-delimiters)
+(use-package rainbow-delimiters
+  :config
   (add-hook 'prog-mode-hook 'rainbow-delimiters-mode))
-
 ;; eldoc
-(when (require 'eldoc)
+(use-package eldoc
+  :config
   (eldoc-mode))
 
 ;; elscreen
-(when (require 'elscreen)
+(use-package elscreen
+  :config
   (elscreen-start))
 
 ;; powerline
-(when (require 'powerline)
+(use-package powerline
+  :config
   (powerline-default-theme))
 ;; theme
-(when (require 'color-theme)
-  (load-theme 'darktooth t))
+(load-theme 'darktooth t)
 
-(when (require 'neotree)
-	   ;; 隠しファイルをデフォルトで表示
-	   (setq neo-show-hidden-files t)
-	   ;; neotree でファイルを新規作成した後、自動的にファイルを開く
-	   (setq neo-create-file-auto-open t)
-	   ;; delete-other-window で neotree ウィンドウを消さない
-	   (setq neo-persist-show t)
-	   ;; キーバインドをシンプルにする
-	   (setq neo-keymap-style 'concise)
-	   ;; windowサイズを可変にする
-	   (setq neo-window-fixed-size nil)
-	   )
+(use-package neotree
+  :commands neotree
+  :init
+  ;; 隠しファイルをデフォルトで表示
+  (setq neo-show-hidden-files t)
+  ;; neotree でファイルを新規作成した後、自動的にファイルを開く
+  (setq neo-create-file-auto-open t)
+  ;; delete-other-window で neotree ウィンドウを消さない
+  (setq neo-persist-show t)
+  ;; キーバインドをシンプルにする
+  (setq neo-keymap-style 'concise)
+  ;; windowサイズを可変にする
+  (setq neo-window-fixed-size nil))
 
-(when (require 'company)
+(use-package company
+  :config
   (global-company-mode t)
   (company-quickhelp-mode t))
 
-(when (require 'go-mode)
-  (require 'company-go)
-  (require 'go-eldoc))
+(use-package go-mode
+  :config
+  (use-package company-go)
+  (use-package go-eldoc))
 
 ;; helm
-(when (require 'helm-config)
-  (when (require 'helm-command)
+(use-package helm
+  :config
+  (use-package helm-config
+	:init
 	(setq helm-autoresize-max-height 0)
 	(setq helm-autoresize-min-height 20)
-	(helm-autoresize-mode 1)
-	(define-key global-map [remap find-file] 'helm-find-files)
-	(define-key global-map [remap occur] 'helm-occur)
-	(define-key global-map [remap list-buffers] 'helm-buffers-list)
-	(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
-	(global-set-key (kbd "M-x") 'helm-M-x)
-	(helm-mode 1)))
+	:config
+	(use-package helm-command
+	  :config (helm-autoresize-mode 1)))
+  :bind (
+	([remap find-file] . helm-find-files)
+	([remap occur] . helm-occur)
+	([remap list-buffers] . helm-buffers-list)
+	([remap dabbrev-expand] . helm-dabbrev)
+	("M-x" . helm-M-x)))
+;; (when (require 'helm-config)
+;;   (when (require 'helm-command)
+;; 	(setq helm-autoresize-max-height 0)
+;; 	(setq helm-autoresize-min-height 20)
+;; 	(helm-autoresize-mode 1)
+;; 	(define-key global-map [remap find-file] 'helm-find-files)
+;; 	(define-key global-map [remap occur] 'helm-occur)
+;; 	(define-key global-map [remap list-buffers] 'helm-buffers-list)
+;; 	(define-key global-map [remap dabbrev-expand] 'helm-dabbrev)
+;; 	(global-set-key (kbd "M-x") 'helm-M-x)
+;; 	(helm-mode 1)))
 
-(require 'magit)
+(use-package magit)
